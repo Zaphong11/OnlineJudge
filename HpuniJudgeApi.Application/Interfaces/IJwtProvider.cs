ce3e5393b5df1cb56534bb3edb2be81ca932de75
Zaphong11 <phongnguyengia82@gmail.com>
2026-07-17 22:38:56 +0700

    Add some comments and improve

--- HpuniJudgeApi.Application/Interfaces/IJwtProvider.cs
+++ HpuniJudgeApi.Application/Interfaces/IJwtProvider.cs
@@ -2,6 +2,10 @@
 
 namespace ToDoAppApi.Application.Interfaces;
 
+/// <summary>
+/// Providing services related to the creation and management of JSON Web Tokens (JWT).
+/// </summary>
+
 public interface IJwtProvider
 {
     string GenerateToken(UserEntity user, IList<string> roles);   
